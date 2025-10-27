8f8235a0ca9a6d9a9f9f3797b02ecc6bcea06797
Vishwajeet-MI <vishwajeet.moodi@gmail.com>
2025-10-27 21:36:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import IAP from './pages/IAP';
+
+function App() {
+  return (
+    <IAP />
+  );
+}
+
+export default App;
---
+++ src/pages/IAP.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IAP = () => {
+  return (
+    <div>
+      IAP Page
+    </div>
+  )
+}
+
+export default IAP
